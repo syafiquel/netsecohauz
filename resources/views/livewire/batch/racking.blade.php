a64d6887116e7c6e8af7e3770b726a95295132dc
syafiquel <syafiqul.syazwan@gmail.com>
2022-11-16 07:20:57 +0000

    fix: racking palette_id null

--- resources/views/livewire/batch/racking.blade.php
+++ resources/views/livewire/batch/racking.blade.php
@@ -29,17 +29,17 @@
                     </thead>
                     <tbody>
                         @foreach($rows as $j => $row)
-                            
                             <tr>
                                 <td>{{ $row }}</td>
                             @foreach($columns as $k => $column)
+                                @php $is_palette = isset(\App\Models\Racking::where([ 'section' => $section, 'row' => $row, 'column' => $column ])->first()->palette_id) ? true : false @endphp
                                 @if(isset($palette_id))
-                                    <td id="{{ $section . $row . $column }}" wire:click="cellClicked('{{ $section . '.' . $row . '.' . $column }}')" 
-                                    style="color:white;{{ (\App\Models\Racking::where([ 'section' => $section, 'row' => $row, 'column' => $column ])->first()->palette_id == null) ? 'background-color:blue;' : 'background-color:red;' }}">
+                                    <td id="{{ $section . $row . $column }}" wire:click="cellClicked('{{ $section . '.' . $row . '.' . $column }}')"
+                                    style="color:white;{{ $is_palette ? 'background-color:blue;' : 'background-color:red;' }}">
                                     {{ $section . $row . $column }}</td>
                                 @else
-                                    <td id="{{ $section . $row . $column }}" wire:click="discardCell('{{ $section . '.' . $row . '.' . $column }}')" 
-                                    style="color:white;{{ (\App\Models\Racking::where([ 'section' => $section, 'row' => $row, 'column' => $column ])->first()->palette_id == null) ? 'background-color:blue;' : 'background-color:red;' }}">
+                                    <td id="{{ $section . $row . $column }}" wire:click="discardCell('{{ $section . '.' . $row . '.' . $column }}')"
+                                    style="color:white;{{ $is_palette ? 'background-color:blue;' : 'background-color:red;' }}">
                                     {{ $section . $row . $column }}</td>
                                 @endif
                             @endforeach
