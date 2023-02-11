ce23ce749d36e91a59577779dab3a506bb43a6d2
syafiquel <syafiqulsyazwan@gmail.com>
2023-02-11 08:53:28 +0800

    updated

--- app/Http/Controllers/BatchController.php
+++ app/Http/Controllers/BatchController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Batch;
 use App\Models\BatchOperation;
-use Illuminate\Support\Facades\Carbon;
+use Illuminate\Support\Carbon;
 
 class BatchController extends Controller
 {
